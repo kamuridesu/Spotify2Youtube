9ea5685d5474eb69f89a94bb6db56a41958ce033
kamuridesu <myk.gata14@gmail.com>
2021-11-16 16:00:24 -0300

    yt on threading and progress

--- SpotifyAuthPage.py
+++ SpotifyAuthPage.py
@@ -5,6 +5,9 @@ import json
 from SpotyApi import SpotifyApi
 import youtube
 
+import threading
+import random
+
 app = Flask(__name__)
 app.secret_key = "secret-key"
 secrets = {}
@@ -14,6 +17,8 @@ with open("credentials.json", "r", encoding="utf-8") as f:
 API_BASE = "https://accounts.spotify.com"
 REDIRECT_URI = "http://localhost:5000/callback"
 SCOPE = 'playlist-modify-private,playlist-modify-public,user-top-read'
+
+THREADS = {}
 
 
 @app.route("/")
@@ -93,15 +98,42 @@ def tracks():
 def migrate():
     try:
         api = SpotifyApi()
+        playlist_id = request.args.get('playlist_id')
         playlists = api.getPlaylists(session["token_info"]['access_token'])
-        tracks = api.getTracks(playlists,
+        playlist = ""
+        for pl in playlists:
+            if pl['id'] == playlist_id:
+                playlist = [pl]
+        if playlist == "":
+            playlist = playlists
+        tracks = api.getTracks(playlist,
                                session["token_info"]['access_token'])
-        playlist = youtube.PlaylistToYoutube()
-        render_template("migrating.html")
-        playlist.migratePlaylist(tracks)
-        return redirect('index')
+
+        thread_id = random.randint(0, 10000)
+        THREADS[thread_id] = youtube.PlaylistToYoutube(tracks)
+        THREADS[thread_id].start()
+        # playlist = youtube.PlaylistToYoutube()
+        # render_template("migrating.html")
+        # playlist.migratePlaylist(tracks)
+        # return redirect('success')
+        return redirect("/migration_success?task_id=" + str(thread_id))
     except (spotipy.exceptions.SpotifyException, KeyError):
         return redirect("auth")
+
+
+@app.route("/progress/<int:task_id>")
+def progress(task_id):
+    global THREADS
+    if THREADS[task_id].done:
+        return "done"
+    return str(THREADS[task_id].progress)
+
+
+@app.route("/migration_success")
+def mg_success():
+    args = request.args.get("task_id")
+    if args:
+        return render_template("migration_success.html", task_id=args)
 
 
 @app.route('/delete')
--- youtube.py
+++ youtube.py
@@ -1,32 +1,51 @@
 # from re import search
 from ytmusicapi import YTMusic
+import threading
+import time
 
 
-def addSongToPlaylist(playlistId, search_query, ytmusic_intance):
-    search_results = ytmusic_intance.search(search_query)
-    for result in search_results:
-        if result['resultType'] == "song":
-            print(result['title'])
-            ytmusic_intance.add_playlist_items(playlistId, [result['videoId']])
-            break
-
-
-class PlaylistToYoutube:
-    def __init__(self) -> None:
+class PlaylistToYoutube(threading.Thread):
+    def __init__(self, playlists_to_import) -> None:
+        super().__init__()
+        self.progress = 0
+        self.done = False
         self.ytmusic = YTMusic('headers_auth.json')
+        self.playlists_to_import = playlists_to_import
+
+    def addSongToPlaylist(self, playlistId, search_query):
+        search_results = self.ytmusic.search(search_query)
+        for result in search_results:
+            if result['resultType'] == "song":
+                print(result['title'])
+                self.ytmusic.add_playlist_items(playlistId,
+                                                [result['videoId']])
+                break
 
     def migratePlaylist(self, playlists_to_import):
-        for playlist in playlists_to_import:
-            playlistId = self.ytmusic.create_playlist(
-                playlist['playlist_name'], 'Imported from Spotify')
-            print("localhost:5000/delete?playlist_id=" + playlistId)
-            print("migrando " + playlist['playlist_name'])
-            # print(playlist['tracks'])
-            print(len(playlist['tracks']))
-            # processes = []
-            for song in playlist['tracks']:
-                search_query = song['name'] + " " + song['artist']
-                addSongToPlaylist(playlistId, search_query, self.ytmusic)
+        debug = True
+        if debug:
+            for playlist in playlists_to_import:
+                for x in playlist['tracks']:
+                    self.progress += 1
+                    time.sleep(0.1)
+            self.done = True
+            return
+        else:
+            for playlist in playlists_to_import:
+                playlistId = self.ytmusic.create_playlist(
+                    playlist['playlist_name'], 'Imported from Spotify')
+                print("localhost:5000/delete?playlist_id=" + playlistId)
+                print("migrando " + playlist['playlist_name'])
+                # print(playlist['tracks'])
+                print(len(playlist['tracks']))
+                # processes = []
+                for song in playlist['tracks']:
+                    self.progress += 1
+                    search_query = song['name'] + " " + song['artist']
+                    self.addSongToPlaylist(playlistId, search_query, self.ytmusic)
+
+    def run(self):
+        self.migratePlaylist(self.playlists_to_import)
 
     def deletePlaylist(self, playlistId):
         self.ytmusic.delete_playlist(playlistId)
